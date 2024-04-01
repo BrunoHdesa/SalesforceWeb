e3093635ee7ff739860bff1ce827c4610bcfe1eb
BrunoH <desabruno2004@gmail.com>
2024-04-01 15:36:55 -0300

    first commit

---
+++ src/styles/colors.ts
@@ -0,0 +1,27 @@
+export const colors = {
+    primary: {
+      main: "#FFC00C",
+    },
+    secondary: {
+      main: "#B2AFA8",
+    },
+    text: {
+      main: "#3A3222",
+      title: "#272a50",
+    },
+    button: {
+      main: "#2E844A",
+      hover:"#327849"
+    },
+    subtitle: {
+      main: "#003056"
+    },
+    backgound: {
+      main: "#B0D4F0",
+    },
+    commom: {
+      white: "#ffffff",
+      black: "#000000",
+    },
+  };
+  
