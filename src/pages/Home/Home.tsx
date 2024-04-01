3808cb1591844d80467a30b62fe4407c4e8e077f
BrunoH <desabruno2004@gmail.com>
2024-04-01 19:54:37 -0300

    adicionando o header

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,14 @@
+import { Layout } from "../../components/Layout/Layout";
+
 export default function Home() {
 
+  const title = "O Poder da Salesforce\nCustomer 360 ao Seu\nAlcance."
   return (
     <>
-
+      <Layout>
+            <h1>{title}</h1>
+            
+      </Layout>
     </>
   );
 }
