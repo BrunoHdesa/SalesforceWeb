ed5cf1c49aa194c5ddd1560fa75274765dcd6a51
BrunoH <desabruno2004@gmail.com>
2024-04-01 22:27:29 -0300

    alterando footer

---
+++ src/components/Footer/Footer.style.ts
@@ -0,0 +1,76 @@
+
+import styled from "styled-components";
+import { colors } from "../../styles/colors";
+
+export const Box = styled.div`
+	display: flex;
+	justify-content: center;
+	padding: 30px 30px 20px 30px;
+	background-color: ${colors.backgound.main};
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	margin-top: 80px;
+`;
+
+export const FooterContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	width: 90%;
+	background: ${colors.backgound.main};
+`;
+
+
+export const Logo = styled.div`
+	display: flex;
+	flex-direction: column;
+	text-align: left;
+	margin-right: 30px;
+
+	& img {
+		width: 80%;
+	}
+`;
+
+export const Column = styled.div`
+	display: flex;
+	flex-direction: column;
+	text-align: left;
+	margin-left: 60px;
+`;
+
+export const Row = styled.div`
+	display: grid;
+	grid-template-columns: repeat(
+		auto-fill,
+		minmax(185px, 2fr)
+	);
+	grid-gap: 10px;
+
+	@media (max-width: 1000px) {
+		grid-template-columns: repeat(
+			auto-fill,
+			minmax(200px, 1fr)
+		);
+	}
+`;
+
+export const FooterLink = styled.a`
+	color: ${colors.commom.black};
+	margin-bottom: 10px;
+	font-size: 12px;
+	text-decoration: none;
+
+	&:hover {
+		color: ${colors.primary.main};
+		transition: 200ms ease-in;
+	}
+`;
+
+export const Heading = styled.p`
+	font-size: 15px;
+	color: ${colors.commom.black};
+	margin-bottom: 14px;
+	font-weight: bold;
+`;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,28 @@
-export default function Footer(){
+import { Box, FooterContainer, Row, Column, FooterLink, Heading, Logo } from "./Footer.style";
+
+const Footer = () => {
     return (
-        <>
-        
-        </>
+        <Box>
+            <Logo>
+                <img src="src\assets\salesforce-simples.png" alt="" />
+            </Logo>
+            <FooterContainer>
+                <Row>
+                    <Column>
+                        <Heading>Sobre nós</Heading>
+                        <FooterLink href="/">
+                            O que é a Salesforce?
+                        </FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Contato</Heading>
+                        <FooterLink href="/contato">
+                            Fale conosco
+                        </FooterLink>
+                    </Column>
+                </Row>
+            </FooterContainer>
+        </Box>
     );
-}
+};
+export default Footer;
